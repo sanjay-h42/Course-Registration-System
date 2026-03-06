0f4d3b543fb6c72e8a2856a1b7697f16166da59c
Elancheral <elanelancheral@gmail.com>
2026-03-06 21:13:41 +0530

    Course registration system

--- Course Registration frontend/myscript.js
+++ Course Registration frontend/myscript.js
@@ -6,6 +6,7 @@ function showCourses(){
         const dataTable = document.getElementById("coursetable")
 
         courses.forEach(course => {
+            
             var row = `<tr>
             <td>${course.courseId}</td>
             <td>${course.courseName}</td>
